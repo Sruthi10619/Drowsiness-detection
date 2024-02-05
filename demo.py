35f814e6ca90c8622b65b363a397b29baa13bf9e
Sruthi10619 <sruthi10619@gmail.com>
2024-02-05 15:25:30 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 def sru(num):
 
 
-    account_sid = 'AC93775dc42b40670cf6835820cb79c098'
-    auth_token = '76a6270b389c2e137a60399a1fadb8f2'
+    account_sid = '################################'
+    auth_token = '###############################'
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         body='Bro wake up you are feeling drowsiness',
